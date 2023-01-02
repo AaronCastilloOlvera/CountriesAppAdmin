4a6f5e18414d54c5a1a91e9445159791db82165c
AaronCastilloOlvera <aaroncastilloolvera@gmail.com>
2023-01-01 20:30:16 -0600

    Add 4 buttons and test jsos.stringify

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const baseUrl = 'http://localhost:7192/'; // Only to test
 
     axios.get("https://localhost:7192/api/Country/")  //`${baseUrl}/flag/1`)
         .then( response => {
-            console.log(response)
-        })
+            const myCodeResp = document.getElementById("my-code");
+
+            myCodeResp.value  = JSON.stringify(response.data, null, 2);
+
+        });
         
 
         
